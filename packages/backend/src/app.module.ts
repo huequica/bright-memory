63b2dca629eedeb0ce52f948714121d0d4e9405b
huequica <dev@huequica.xyz>
2023-05-28 20:56:42 +0900

    register root app to created modules

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from '@/src/users/users.module';
+import { AuthModule } from '@/src/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:password@127.0.0.1:53251/bright-memory',
+      { authSource: 'admin' }
+    ),
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
