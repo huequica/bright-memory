5b74bd05ccd87c03c6f73cfef492723fd5ff4230
huequica <dev@huequica.xyz>
2023-06-10 06:03:35 +0900

    WIP(backend/entry): create search@POST endpoint

--- packages/backend/src/entry/entry.controller.ts
+++ packages/backend/src/entry/entry.controller.ts
@@ -9,15 +9,15 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { EntryService } from './entry.service';
-import { CreateEntryDTO } from '@/schemas/entry/entry.dto';
+import { CreateEntryDTO, SearchEntryDTO } from '@/schemas/entry/entry.dto';
 import { UserDocument } from '@/schemas/user/user.schema';
 import { EntryDocument } from '@/schemas/entry/entry.schema';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('entry')
 export class EntryController {
   constructor(private readonly entryService: EntryService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Post('new')
   protected async createEntry(
     @Body() createEntry: CreateEntryDTO,
@@ -26,12 +26,20 @@ export class EntryController {
     return this.entryService.create(createEntry.url, req.user);
   }
 
-  @UseGuards(AuthGuard('jwt'))
   @Get(':entryId')
   protected async find(
     @Param('entryId') entryId: string,
     @Request() req: { user: UserDocument }
   ): Promise<EntryDocument> {
     return this.entryService.find(entryId, req.user);
+  }
+
+  @Post('search')
+  async search(
+    @Body() search: SearchEntryDTO,
+    @Request() req: { user: UserDocument }
+  ) {
+    console.log(search);
+    return Promise.resolve(null);
   }
 }
