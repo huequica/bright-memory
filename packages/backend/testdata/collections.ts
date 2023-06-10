cebbd36980b0b351d15c40be47141ffe7d73728b
huequica <dev@huequica.xyz>
2023-06-10 09:21:38 +0900

    feature(testdata): create entry testdata

--- packages/backend/testdata/collections.ts
+++ packages/backend/testdata/collections.ts
@@ -1,3 +1,4 @@
 import { usersCollection } from '@/testdata/Users';
+import { entriesCollection } from '@/testdata/Entry';
 
-export const insertCollections = [usersCollection];
+export const insertCollections = [usersCollection, entriesCollection];
