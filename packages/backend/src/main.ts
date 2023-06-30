abb8c08fb833ccc7aed28e39195b6ecd367a4e10
huequica <dev@huequica.xyz>
2023-07-01 07:56:51 +0900

    feature(general/root): setup swagger document

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Bright-Memory-API')
+    .setDescription('Bright-Memory Backend service documents')
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(53250);
 }
 
