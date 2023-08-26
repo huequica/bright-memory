e6cea653e08024319d449b9e8f2b849115baafb2
huequica <dev@huequica.xyz>
2023-08-27 00:16:34 +0900

    循環参照してんじゃねえかばーーーーーーーか

--- packages/general/components/Button/Button.tsx
+++ packages/general/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
+import { Button as MUIButton } from '@mui/material';
 import { ButtonProps } from '@mui/material';
 
 export const Button: FC<ButtonProps> = (props) => {
-  return <Button {...props} />;
+  return <MUIButton {...props} />;
 };
