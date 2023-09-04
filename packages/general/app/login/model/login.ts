680811a804b9f187020f2966f8d2c0180b0aecbd
huequica <dev@huequica.xyz>
2023-09-04 10:31:05 +0900

    replace `noEmpty` to `min` in LoginFormSchema

--- packages/general/app/login/model/login.ts
+++ packages/general/app/login/model/login.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const loginFormSchema = z.object({
-  loginName: z.string().email(),
-  password: z.string().nonempty(),
+  loginName: z.string().trim().min(1),
+  password: z.string().trim().min(1),
 });
