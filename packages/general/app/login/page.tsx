e3761c4988b3ff0547ebdd07a757b1f9c528307a
huequica <dev@huequica.xyz>
2023-12-03 19:24:57 +0900

    replace callback redirect to router.push

--- packages/general/app/login/page.tsx
+++ packages/general/app/login/page.tsx
@@ -3,21 +3,26 @@ import { NextPage } from 'next';
 import { signIn } from 'next-auth/react';
 import { LoginForm } from './components';
 import { useAlert } from '@/components';
+import { useRouter } from 'next/navigation';
 
 const LoginPage: NextPage = () => {
   const { open } = useAlert();
+  const router = useRouter();
 
   return (
     <LoginForm
       onSubmit={async (values) => {
         signIn('signIn', {
-          callbackUrl: '/',
+          redirect: false,
           id: values.loginName,
           password: values.password,
         })
           .then((res) => {
             if (res?.error) {
               open('error', 'error occurred');
+            } else {
+              open('success', 'login successfully');
+              router.push('/');
             }
           })
           .catch((error) => {
