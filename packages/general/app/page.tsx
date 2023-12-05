6887710bd8e0030523c2140c234f46c4ce38fca1
huequica <dev@huequica.xyz>
2023-12-06 04:54:58 +0900

    implement page

--- packages/general/app/page.tsx
+++ packages/general/app/page.tsx
@@ -1,6 +1,22 @@
+'use client';
 import { NextPage } from 'next';
+import { useEntries } from '@/app/_hooks/entry';
+import { useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
+  const session = useSession();
+
+  const isSessionReady = !!session.data;
+
+  const { data, isLoading } = useEntries(
+    { size: 20, pageNumber: 0 },
+    isSessionReady
+  );
+  if (!data || isLoading) {
+    return <>Loading...</>;
+  }
+
+  console.log(data);
   return <>hogehoge</>;
 };
 
