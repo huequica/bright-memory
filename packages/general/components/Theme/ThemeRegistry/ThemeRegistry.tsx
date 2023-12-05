22b6eb52499e51482c6643102f504bd81f7abf57
huequica <dev@huequica.xyz>
2023-12-06 04:01:58 +0900

    insert query client provider

--- packages/general/components/Theme/ThemeRegistry/ThemeRegistry.tsx
+++ packages/general/components/Theme/ThemeRegistry/ThemeRegistry.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { NextAppDirEmotionCacheProvider } from './EmotionCache';
 import { defaultTheme } from './theme';
 import { AlertProvider } from '@/components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 interface Props {
   children: ReactNode;
@@ -16,13 +17,16 @@ export const ThemeRegistry: FC<Props> = ({
 }: {
   children: ReactNode;
 }) => {
+  const queryClient = new QueryClient();
   return (
     <>
       <CssBaseline />
       <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
-        <AlertProvider>
-          <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
-        </AlertProvider>
+        <QueryClientProvider client={queryClient}>
+          <AlertProvider>
+            <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+          </AlertProvider>
+        </QueryClientProvider>
       </NextAppDirEmotionCacheProvider>
     </>
   );
