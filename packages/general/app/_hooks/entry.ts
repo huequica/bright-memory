86e98d8449c4852aa7c6119b08745c1b4f3d9857
huequica <dev@huequica.xyz>
2023-12-07 00:08:45 +0900

    remove unnecessary implementation

--- packages/general/app/_hooks/entry.ts
+++ packages/general/app/_hooks/entry.ts
@@ -1,32 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import { entryApi } from '@/lib';
 
-export const useEntries = (
-  {
-    size = 30,
-    pageNumber,
-  }: {
-    size?: number;
-    pageNumber: number;
-  },
-  enabled?: boolean
-) => {
+export const useEntries = ({
+  size = 20,
+  pageNumber,
+}: {
+  size?: number;
+  pageNumber: number;
+}) => {
   return useQuery({
     queryKey: ['entries'],
     queryFn: async () => {
-      const { data } = await entryApi.search(
-        {
-          pagination: {
-            size: size,
-            pageNumber: pageNumber,
-            sort: 'updatedAt',
-          },
+      const { data } = await entryApi.search({
+        pagination: {
+          size: size,
+          pageNumber: pageNumber,
+          sort: 'updatedAt',
         },
-        {}
-      );
+      });
 
       return data;
     },
-    enabled,
   });
 };
--- packages/general/app/page.tsx
+++ packages/general/app/page.tsx
@@ -1,22 +1,13 @@
 'use client';
 import { NextPage } from 'next';
 import { useEntries } from '@/app/_hooks/entry';
-import { useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
-  const session = useSession();
-
-  const isSessionReady = !!session.data;
-
-  const { data, isLoading } = useEntries(
-    { size: 20, pageNumber: 0 },
-    isSessionReady
-  );
+  const { data, isLoading } = useEntries({ pageNumber: 0 });
   if (!data || isLoading) {
     return <>Loading...</>;
   }
 
-  console.log(data);
   return <>hogehoge</>;
 };
 
